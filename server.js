18d505751e5c3bd5dc73d65f91f8ed6055e92715
Vsoto7697 <109629515+Vsoto7697@users.noreply.github.com>
2022-12-22 18:20:47 -0500

    updated all handlebars

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const path = require('path');
 const exphbs = require('express-handlebars');
 const hbs = exphbs.create({});
+const routes = require('./controllers/');
 
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
